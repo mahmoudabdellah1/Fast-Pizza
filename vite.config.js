7b4bf79cb9d0ec9b8a6d0afccb9a1f73cf44e286
Mahmoud Abdellah <113348238+mahmoudabdellah1@users.noreply.github.com>
2026-03-02 17:18:34 +0200

    Fix base path in Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import eslint from "vite-plugin-eslint";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), eslint()],
-    base: "/Fast-Pizza",
+    base: "/Fast-Pizza/",
 });
